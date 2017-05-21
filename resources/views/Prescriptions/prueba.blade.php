de2dee7dc820e004a5516cc17fb83eb8084f110e
texsmv <texs.mv@gmail.com>
2017-05-20 21:25:40 -0500

    Pruebas template

---
+++ resources/views/Prescriptions/prueba.blade.php
@@ -0,0 +1,15 @@
+
+
+@extends('layouts.prescriptionsTemplate')
+@section('title', 'Page Title')
+
+
+   
+   @section('content')      
+      <p>This is my body content.</p>
+      <p>This is my body content.</p>
+      <p>This is my body content.</p>
+      <p>This is my body content.</p>
+      <p>This is my body content.</p>
+      <p>This is my body content.</p>
+   @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('prueba', function(){
+	return View::make('Prescriptions.prueba');
+});
