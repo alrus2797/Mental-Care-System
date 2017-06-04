ac5266ff97dbcf4dd576363d892490daeffd02e6
Kath17 <kathy.uc.17@gmail.com>
2017-06-04 13:48:50 -0500

    presentaciones

--- resources/views/Prescriptions/Medicamentos/presentacion.blade.php
+++ resources/views/Prescriptions/Medicamentos/presentacion.blade.php
@@ -1,53 +1,106 @@
-
 @extends('layouts.prescriptionsTemplate')
 
 @section('title','Agregar presentacion')
 
 @section('content')
 
+<div class="row">
 
-<form method="post" id="form" action="{{asset('presentacion')}}">
+<form method="post" id="form" action="{{asset('medicamentos/'.$medicamento->id.'/presentacion')}}">
 	{{ csrf_field()}}
 
-	<div class="row">
-  		<div class="col-md-4 col-md-offset-3">
+		<div class="col-md-10">
 
-		<h2><p class="text-center">  Agregar presentación </p></h2>
-		<br>
+				<h2><p class="text-center">  Agregar presentación </p></h2>
+				<br>
 
-	  <div class="row">
-			<div class="col-sm-12">
-				<label for="nomPres" >Nombre de la Presentación: </label>
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1">
-							<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-						</span>
-			  		<input class="form-control" type="text" name ="nomPres" id="nomPres" placeholder="Ingrese nombre de la presentacion" required="">
-				</div>
-			    <div id="EnomPres" ></div>
-			</div>
-	  </div><br>
+			<div class="col-md-1">
+			</div>				
 
-	  <div class="row">
-			<div class="col-sm-12">
-				<label for="Descripcion" > Descripción: </label>
-				<div class="input-group">
-						<span class="input-group-addon" id="basic-addon1">
-							<span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
-						</span>
-	        		<textarea class="form-control" placeholder="Ingrese la descripción de la presentación"  name="Descripcion" id="Descripcion" rows="5" required="true"></textarea>
-				</div>
-			    <div id="EnomDescripcion" ></div>
-			</div>
-	  </div><br>
+			<div class="col-md-6">	
 
-	      <div class="text-center">
-	      	<button class="btn btn-lg btn-primary btn-primary" type="submit" value="Submit"> Agregar presentacion</button>
-	      </div>
+			  <div class="row">
+					<div class="col-sm-12">
+						<label for="nomPres" >Nombre de la Presentación: </label>
+						<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">
+									<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+								</span>
+					  		<input class="form-control" type="text" name ="nomPres" id="nomPres" placeholder="Ingrese nombre de la presentacion" required="">
+						</div>
+					    <div id="EnomPres" ></div>
+					</div>
+			  </div><br>
+
+			  <div class="row">
+			  		<div class="col-sm-12">
+			  			<label for="modelo" > Modelo de presentación: </label>
+				   			<select name="modelo" type="text" class="form-control" id="id_modelo">
+			                	<option value="" >Seleccionar modelo de presentación:</option>
+			                		@foreach ($modelos as $modelo)
+			          				<option value="{{$modelo->id}}" >{{$smodelo->nombre}}</option>
+			        				@endforeach
+			                  	<option value="{$modelo->id}" >{$smodelo->nombre}</option>
+			        		</select>
+		        	</div>
+		      </div><br>
+
+			  <div class="row">
+					<div class="col-sm-12">
+						<label for="Descripcion" > Descripción: </label>
+						<div class="input-group">
+								<span class="input-group-addon" id="basic-addon1">
+									<span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+								</span>
+			        		<textarea class="form-control" placeholder="Ingrese la descripción de la presentación"  name="Descripcion" id="Descripcion" rows="5" required="true"></textarea>
+						</div>
+					    <div id="EnomDescripcion" ></div>
+					</div>
+			  </div><br>
+
+			      <div class="text-center">
+			      	<button class="btn btn-lg btn-primary btn-primary" type="submit" value="Submit"> Agregar presentacion</button>
+			      </div>
 
 		</div>
+
+		<div class="col-md-4">
+			<table class="table table-hover">
+			    <thead>
+			      <tr>
+			        <th>Presentaciones</th>
+			      </tr>
+			    </thead>
+
+			    <tbody>
+			    	<tr>
+						<td>{$medicamento->presentaciones}</td>
+				    </tr>
+			    </tbody>
+			  </table>
+		</div>
+
 	</div>
 
 </form>
 
+<form method="get" action="{{asset('presentaciones')}}">
+	{{ csrf_field()}}
+
+	<div class="row">
+		<div class="col-md-10">
+			<div class="col-md-1">	</div>		
+			<div class="col-md-6">	</div>				
+
+			<div class="col-md-4">	
+					<div class="text-center">
+						<button class="btn btn-success btn-lg" type="submit" value="Submit"> Finalizar</button>
+					</div>
+			</div>
+		</div>
+	</div>
+	<br><br><br>
+</form>
+
+</div>
 @endsection
