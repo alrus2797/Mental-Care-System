44afbfc862f77f867f603be0580e895a18e6b0cb
luisbch9 <luisbch9@gmail.com>
2017-06-12 23:21:50 -0500

    librerias

--- resources/views/layouts/prescriptionsTemplate.blade.php
+++ resources/views/layouts/prescriptionsTemplate.blade.php
@@ -4,9 +4,9 @@
   <title>MC - @yield('title')</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
+  <script src="{{ asset('js/jquery.min.css')}}"></script>
+  <script src="{{ asset( 'js/bootstrap.min.js ')}}"></script>
   <link href="{{asset('css/template.css')}}" rel="stylesheet" type="text/css"></link>
 </head>
 <body>
@@ -53,7 +53,7 @@
         <span class="icon-bar"></span>
       </button>      
     </div>
-      @include('layouts.sidebar')
+    @yield('sidebar')
   </div>
 </nav>
 
