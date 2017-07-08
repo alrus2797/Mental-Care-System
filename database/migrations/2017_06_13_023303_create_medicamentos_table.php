6d28db685295622c8ceb24f47203feea0659f1b5
lehi10 <lehi_10@hotmail.com>
2017-07-08 09:18:08 -0500

    New update migrations
    Migratios update- Solving fixes

--- database/migrations/2017_06_13_023303_create_medicamentos_table.php
+++ database/migrations/2017_06_13_023303_create_medicamentos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMedicamentosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('medicamentos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nombre',60);
+            $table->string('descripcion',255);
+            $table->string('efecSecundarios',255);
+            $table->string('adversos',255);
+
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('medicamentos');
     }
 }
--- database/migrations/2017_06_16_001147_create_pacientes_table.php
+++ database/migrations/2017_06_16_001147_create_pacientes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePacientesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pacientes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('historiaclinica',100);
+            $table->string('apellidopaterno',100);
+            $table->string('apellidomaterno',100);
+            $table->string('nombres',100);
+            $table->string('dni',9);
+            $table->string('direccion',200);
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pacientes');
     }
 }
