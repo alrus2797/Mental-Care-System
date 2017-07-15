991cf0b5ba063555b7e94c4997726aeec181d678
DESKTOP-DFDJLJB\Fran <franklincncr@gmail.com>
2017-07-15 13:20:53 -0400

    tabla reportes para almacenar informes en pdf

---
+++ database/migrations/2017_07_17_101120_create_reporte_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reporte', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre',30);
+            $table->date('fecha');
+            $table->string('tipo',7);            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reporte');
+    }
+}
